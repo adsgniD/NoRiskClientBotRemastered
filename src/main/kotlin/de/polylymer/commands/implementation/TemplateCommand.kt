b1f07e0610d0d45011c60611782ddcf44a6ca189
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-17 18:58:49 +0100

    Create TemplateCommand.kt

---
+++ src/main/kotlin/de/polylymer/commands/implementation/TemplateCommand.kt
@@ -0,0 +1,18 @@
+package de.polylymer.commands.implementation
+
+import de.polylymer.commands.SlashCommand
+import dev.kord.common.annotation.KordPreview
+import dev.kord.core.entity.interaction.Interaction
+
+@KordPreview
+object TemplateCommand : SlashCommand(
+    name = "",
+    description = "Get the template to design your own cape"
+) {
+
+    override suspend fun handleCommand(interaction: Interaction) {
+        interaction.acknowledge(true)
+        interaction.channel.createMessage("https://cdn.discordapp.com/attachments/774273183804948500/811656301616169030/template.png")
+    }
+
+}
