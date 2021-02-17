a2c3a9f187a460fb124649d8c8431c6ec45489db
polylymer <74833245+polylymer@users.noreply.github.com>
2021-02-17 17:51:16 +0100

    structured description and mentioned channel with method

--- src/main/kotlin/de/polylymer/commands/implementation/CapeCommand.kt
+++ src/main/kotlin/de/polylymer/commands/implementation/CapeCommand.kt
@@ -3,6 +3,7 @@ package de.polylymer.commands.implementation
 import de.polylymer.commands.SlashCommand
 import dev.kord.common.Color
 import dev.kord.common.annotation.KordPreview
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.followUp
 import dev.kord.core.entity.interaction.Interaction
 import dev.kord.rest.builder.message.EmbedBuilder
@@ -23,11 +24,14 @@ object CapeCommand : SlashCommand(
                 color = Color(0, 251, 255)
                 field {
                     name = "Folgende Schritte erklären dir wie du korrekt ein Cape einreichst!"
-                    value = "Lade dir zuerst das Template aus #faq oder mit /template in #botcommands herunter.\n" +
-                            "Bearbeite das Cape mit Programmen wie z.B paint.net\n" +
-                            "Gehe auf den Minecraft Server kitpvp.de und gebe /cape ein\n" +
-                            "Klicke auf den Link im Chat und lade dort dein bearbeitetes Template hoch\n" +
-                            "Fertig! Habe Geduld und warte bis dein Cape geprüft wurde. In #capelog kannst du überprüfen, ob dein Cape bereits überprüft wurde"
+                    value = "1. Lade dir zuerst das Template aus " + interaction.guild.getChannel(
+                        Snowflake("774292827524956181")).mention + "oder mit /template in" + interaction.guild.getChannel(
+                        Snowflake("774273609467691018")).mention + "herunter.\n" +
+                            "2. Bearbeite das Cape mit Programmen wie z.B paint.net\n" +
+                            "3. Gehe auf den Minecraft Server kitpvp.de und gebe /cape ein\n" +
+                            "4. Klicke auf den Link im Chat und lade dort dein bearbeitetes Template hoch\n" +
+                            "5. Fertig! Habe Geduld und warte bis dein Cape geprüft wurde. In" + interaction.guild.getChannel(
+                        Snowflake("774295768181899325")).mention + "kannst du überprüfen, ob dein Cape bereits überprüft wurde"
                 }
             }
         }
