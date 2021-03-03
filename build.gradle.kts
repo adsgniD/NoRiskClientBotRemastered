dc838172676a434937d689cfc616c8ca9906764e
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-03-03 20:18:05 +0100

    absolut geballert
    Tretti, was geht ab? Ich bin auch grad hier in Berlin gelandet
    Ähm, ich kümmer' mich darum
    Geiler Tune!“
    
    In meinem Tracksuit mit drei Streifen, gekommen, um zu bleiben
    Lass die Flasche kreisen, uns Geschichte schreiben
    Trag' die Brille in der Nacht, hunderttausend Watt
    Ballern ausm Sub (KitschKrieg)
    
    Die ganze Nacht auf Tour
    Treff uns aufm Flur
    Meine Jungs rauchen pur
    Rund um die Uhr
    Aus den Boxen Shabba Rankin'
    Frauen treiben mich in den Wahnsinn
    Keinen Plan, wie das hier anfing
    Doch Gott sei Dank, dass wir da sind
    
    Gott sei Dank haben wir die Ausfahrt nicht verpasst, und
    Gott sei Dank sind alle außerhalb vom Knast, und
    Heh, komm, wir kaufen uns einen Schnaps
    Rauchen auf dem Dach mit Ausblick auf die Stadt
    Mein Status, der Brabus, die Uhr hier musst du dir verdienen
    Lass sie blasen, danach schwimm' ich Bahnen im Pool voll mit Lean
    Die Taschen sind gestopft, wer hätte das gedacht?
    Inkasso hat geklopft, kam mir vor wie letzte Nacht
    Heute kaufe ich mir Schmuck und verliere ihn im Club
    Aber Digga, scheißegal, Hauptsache, alle sind gesund (187!)
    
    Die ganze Nacht auf Tour
    Treff uns aufm Flur
    Meine Jungs rauchen pur
    Rund um die Uhr
    Aus den Boxen Shabba Rankin'
    Frauen treiben mich in den Wahnsinn
    Keinen Plan, wie das hier anfing
    Doch Gott sei Dank, dass wir da sind
    
    Die ganze Nacht auf Tour, seh' keinen andern Sinn
    Alles geht so schnell, aber kann auch einfach sein, dass ich zu langsam bin
    Hab' eine Hand an meinem Drink, mische Fanta und Gin
    Doch verliere mich in mei'm Kopf, als wär' MDMA drin
    Mein Leben ist 'n brandneuer Aston Martin
    Alle suchen sich den besten Platz drin
    Gott sei Dank haben wir Money, Money, Money
    Und die Träume, die wir hatten, werden wahr
    Früher hatten wir kein Money, Money, Money
    Kauften reduzierte Ware bei Spar, Gott sei Dank nun ein Star
    Komm, wir gehen
    
    Die ganze Nacht auf Tour
    Treff uns aufm Flur
    Meine Jungs rauchen pur
    Rund um die Uhr
    Aus den Boxen Shabba Rankin'
    Frauen treiben mich in den Wahnsinn
    Keinen Plan, wie das hier anfing
    Doch Gott sei Dank, dass wir da sind
    
    Drat drat, keine Schüsse, Korken knallen
    Alle happy, keine Sorgenfalten
    Häng' im Club, nur mit Originalen
    Beste Budds in den Taschen, lass uns teilen
    Das erste Mal im Plus, Jahresende
    Das erste Mal im Plus, Jahresende
    Gönn' allen meinen Besten Geschenke
    Und meiner Braut Schmuck, represente
    Christopher Wallace, alles wie im Traum
    Hits, Gigs, Frauen
    Non-stop bis zum Morgengrauen
    Ernte Früchte – Mangobaum
    
    Die ganze Nacht auf Tour
    Treff uns aufm Flur
    Meine Jungs rauchen pur
    Rund um die Uhr
    Aus den Boxen Shabba Rankin'
    Frauen treiben mich in den Wahnsinn
    Keinen Plan, wie das hier anfing
    Doch Gott sei Dank, dass wir da sind

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,10 @@ plugins {
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.serialization") version "1.4.21"
     application
-    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
-
-project.setProperty("mainClassName", "de.polylymer.Manager")
-
-val JVM_VERSION = JavaVersion.VERSION_11
-val JVM_VERSION_STRING = JVM_VERSION.versionString
 
 group = "de.polylymer"
 version = "1.0-SNAPSHOT"
-
 
 repositories {
     mavenCentral()
@@ -35,22 +28,12 @@ dependencies {
 }
 
 application {
-    mainClass.set("de.polylymer.ManagerKht")
+    mainClass.set("de.polylymer.ManagerKt")
 }
 
-val JavaVersion.versionString
-    get() = majorVersion.let {
-        val version = it.toInt()
-        if (version <= 10) "1.$it" else it
-    }
-
-fun org.jetbrains.kotlin.gradle.tasks.KotlinCompile.configureJvmVersion() {
-    kotlinOptions.jvmTarget = JVM_VERSION_STRING
-}
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    configureJvmVersion()
-    configureJvmVersion()
+    kotlinOptions.jvmTarget = "11"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
