e42e17006d808770275e36c8b1c0ba2095503609
mooziii <mooz@moozunity.de>
2021-03-13 12:55:16 +0100

    fix THE BIGGEST FUCKSHIT OF THE WOLRD 2

--- src/main/kotlin/de/polylymer/commands/implementation/CapeCommand.kt
+++ src/main/kotlin/de/polylymer/commands/implementation/CapeCommand.kt
@@ -1,5 +1,6 @@
 package de.polylymer.commands.implementation
 
+import de.polylymer.KordEXT.guild
 import de.polylymer.commands.SlashCommand
 import dev.kord.common.Color
 import dev.kord.common.annotation.KordPreview
@@ -15,26 +16,26 @@ object CapeCommand : SlashCommand(
     description = "Get a description how to upload your custom cape"
 ) {
     override suspend fun handleCommand(interaction: Interaction) {
-        interaction.acknowledge(true).followUp {
+        interaction.acknowledge().followUp {
             embed {
                 title = "Custom Cape"
                 val thumb = EmbedBuilder.Thumbnail()
-                thumb.url = interaction.getGuild().getIconUrl(Image.Format.GIF)!!
+                thumb.url = interaction.guild().getIconUrl(Image.Format.GIF)!!
                 thumbnail = thumb
                 val foot = EmbedBuilder.Footer()
-                foot.icon = interaction.getGuild().getIconUrl(Image.Format.GIF)!!
-                foot.text = interaction.getGuild().name
+                foot.icon = interaction.guild().getIconUrl(Image.Format.GIF)!!
+                foot.text = interaction.guild().name
                 footer = foot
                 color = Color(0, 251, 255)
                 field {
                     name = "Folgende Schritte erklären dir wie du korrekt ein Cape einreichst!"
-                    value = "1. Lade dir zuerst das Template aus " + interaction.guild.getChannel(
-                        Snowflake("774292827524956181")).mention + " oder mit /template in " + interaction.guild.getChannel(
+                    value = "1. Lade dir zuerst das Template aus " + interaction.guild().getChannel(
+                        Snowflake("774292827524956181")).mention + " oder mit /template in " + interaction.guild().getChannel(
                         Snowflake("774273609467691018")).mention + " herunter.\n" +
                             "2. Bearbeite das Cape mit Programmen wie z.B paint.net\n" +
                             "3. Gehe auf den Minecraft Server kitpvp.de und gebe /cape ein\n" +
                             "4. Klicke auf den Link im Chat und lade dort dein bearbeitetes Template hoch\n" +
-                            "5. Fertig! Habe Geduld und warte bis dein Cape geprüft wurde. In " + interaction.guild.getChannel(
+                            "5. Fertig! Habe Geduld und warte bis dein Cape geprüft wurde. In " + interaction.guild().getChannel(
                         Snowflake("774295768181899325")).mention + " kannst du überprüfen, ob dein Cape bereits überprüft wurde"
                 }
             }
