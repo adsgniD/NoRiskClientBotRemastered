f89824b37acd114d7485b95793054fc37425cf54
MatrixCodeDE <matr.xonyt@gmail.com>
2021-06-01 18:14:36 +0200

    Fix
    Zeilenumbruch gefixt

--- src/main/kotlin/de/polylymer/commands/implementation/TemplateCommand.kt
+++ src/main/kotlin/de/polylymer/commands/implementation/TemplateCommand.kt
@@ -13,7 +13,7 @@ object TemplateCommand : SlashCommand(
 
     override suspend fun handleCommand(interaction: Interaction) {
         interaction.ackowledgePublic().followUp {
-            content = "Achtung: Der Capeserver ist im Moment nicht erreichbar!"
+            content = "Achtung: Der Capeserver ist im Moment nicht erreichbar! \n"
             content = "https://cdn.discordapp.com/attachments/788821736815853608/827167628745375744/template2.png"
         }
     }
