4cec7749aed26a5f6dad844f96da1163abb73367
adsgniD <77272661+adsgniD@users.noreply.github.com>
2021-06-03 19:05:49 +0200

    Add files via upload

---
+++ src/main/kotlin/de/polylymer/commands/implementation/CreateCapeCommand.kt
@@ -0,0 +1,21 @@
+package de.polylymer.commands.implementation
+import de.polylymer.commands.SlashCommand
+import dev.kord.common.annotation.KordPreview
+import dev.kord.core.behavior.interaction.followUp
+import dev.kord.core.entity.interaction.Interaction
+import java.nio.file.Path
+
+@KordPreview
+object CreateCapeCommand : SlashCommand(
+    name = "createcape",
+    description = "creates a cape\n-by adsgniD"
+) {
+    override suspend fun handleCommand(interaction: Interaction) {
+        interaction.ackowledgePublic().followUp {
+            val process = ProcessBuilder("python3", "../CreateCape/CapeManager", ).start()
+            var filePath = Path get("../CreateCape/out.png");
+
+            addFile(path = filePath )
+        }
+    }
+}
